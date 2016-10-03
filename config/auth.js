9e55989ef8dadd8ae04f056d62759d99b4da9d90
Ryan Greuel <ryangreuel@gmail.com>
2016-10-03 16:10:27 -0500

    reset client secret for google

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
 
 	'googleAuth' : {
 		'clientID'		: '588710482497-dflo46h1hcmedbbb9gn4f6ni08pgbnn1.apps.googleusercontent.com',
-		'clientSecret'	: 'LqPMXDPEYRO_ZSuIxwUg4aji',
+		'clientSecret'	: 'OCIA617veaeoZo5o21oHhWLe',
 		'callbackURL'	: 'http://localhost:8080/auth/google/callback'
 	}
 };
